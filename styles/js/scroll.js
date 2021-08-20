9047ea0af7fc8c033866260fe2d12a74dac5e6e8
irvin-codes <irvin0329@outlook.com>
2021-08-20 17:27:18 -0600

    added quote section,contact section and footer

--- styles/js/scroll.js
+++ styles/js/scroll.js
@@ -1,12 +1,25 @@
 const header = document.getElementById("main-header");
+const linkHome = document.getElementById("link-home");
+const linkAbout = document.getElementById("link-about");
+const linkSkills = document.getElementById("link-skills");
+const linkContact = document.getElementById("link-contact");
 
 function scrollHeaderTop(){
     if (window.scrollY === 0){
         
         header.classList.remove("scroll-js")
+        linkHome.classList.remove("color-change")
+        linkAbout.classList.remove("color-change")
+        linkSkills.classList.remove("color-change")
+        linkContact.classList.remove("color-change")
     }
     else if(window.scrollY > 1){
         header.classList.add("scroll-js")
+        linkHome.classList.add("color-change")
+        linkAbout.classList.add("color-change")
+        linkSkills.classList.add("color-change")
+        linkContact.classList.add("color-change")
+       
     }
 }
 
